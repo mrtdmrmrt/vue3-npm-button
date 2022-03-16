a0781d9036488bd9278437c53d9fd66fe776269a
Mert Demir (Berkut Teknoloji) <extmertde@d-teknoloji.com.tr>
2022-03-16 18:53:53 +0300

    first commit

---
+++ src/components/MdButtonV3.vue
@@ -0,0 +1,13 @@
+<template>
+  <button class="md-button">
+    <slot />
+  </button>
+</template>
+<style scoped>
+.md-button {
+  background-color: #43b883;
+  color: white;
+  outline: none;
+  border: none;
+}
+</style>
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import MdButtonV3 from "./components/MdButtonV3.vue";
+export default {
+    install: (app,options)=>{
+        app.component("md-button-v3", MdButtonV3);
+    }
+}
